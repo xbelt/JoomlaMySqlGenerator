a893eab69ab9a4ebafb4f5bfc06e3cf1a5bc382a
Lukas Häfliger <lukas@haefliger.email>
2015-02-08 12:54:11 +0100

    add default value

--- JoomlaMySqlGenerator/FillData.Designer.cs
+++ JoomlaMySqlGenerator/FillData.Designer.cs
@@ -50,6 +50,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(177, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "categories;diseases";
             // 
             // label2
             // 
